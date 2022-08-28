bfa786fb458ec6ecdaad02a8cc947d0a4ec8a21d
João Victor Cunha <joaovictorcunha8@gmail.com>
2022-08-28 03:29:34 -0300

    descktop navigation added

---
+++ src/components/header/DescktopNavigarion.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function DescktopNavigarion() {
+  return (
+    <nav>
+        <ul className='flex gap-4 text-white'>
+            <li>INICIO</li>
+            <li>A EMPRESA</li>
+            <li>SERVIÇOS</li>
+            <li>NOTÍCIAS E ATUALIZAÇÕES</li>
+            <li>CONTATO</li>
+        </ul>
+    </nav>
+  )
+}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,7 @@ import logo from "../../assets/logo01.png"
 import Hamburger from 'hamburger-react'
 import { MobileNavigation } from './MobileNavigation'
 import useWindowDimensions from '../../lib/WindowsDimentions'
+import { DescktopNavigarion } from './DescktopNavigarion'
 
 
 
@@ -12,7 +13,7 @@ export function Header() {
   const {height, width} = useWindowDimensions()
 
   return (
-    <header className='fixed w-full z-10  lg:px-[10%] 2xl:px-[20%]'>
+    <header className={`fixed w-full z-10  lg:px-[10%] 2xl:px-[20%]`}>
       <div className='flex items-center justify-between mx-5 mt-8'>
         <img src={logo} alt="" className='w-28 cursor-pointer'/>
 
@@ -20,7 +21,7 @@ export function Header() {
         { //cria uma condicional onde se a tela for menor que 768px renderiza o hamburger menu
           width < 768?
             <div className='absolute z-20 right-5' onClick={()=>setIsOpen(!isOpen)}><Hamburger color='#2e612f'/></div>
-            : null
+            : <DescktopNavigarion/>
         }
 
         {
