49d42ecec76008f350816e98abd2a105b0768e5f
João Victor Cunha <joaovictorcunha8@gmail.com>
2022-08-30 22:30:09 -0300

    services section with 2xl screen

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -3,15 +3,17 @@ import { Card } from './Card'
 //CRIAR OBJETO DE AREAS
 export function Services() {
   return (
-    <section className='flex flex-col items-center bg-[#B1D7B4] py-8 px-8 mt-10'>
-        <h2 className='text-3xl  border-b-4 mb-12 border-brand-h2'>SERVIÇOS</h2>
-        <div className=' grid gap-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-6 '>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+    <section className='flex flex-col items-center bg-[#B1D7B4] py-8 px-8 mt-10 absolute w-full 2xl:h-96'>
+        <h2 className='text-3xl  border-b-4 mb-20 border-brand-h2'>SERVIÇOS</h2>
+        <div>
+          <div className=' grid gap-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-6 2xl:relative '>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+          </div>        
         </div>
         
     </section>
